5fb5d2c1f153b6b61c8c24374815f53462084aa9
omarnagah <omarnagah3@gmail.com>
2021-11-12 17:47:19 +0200

    Enhancing code readability.

--- app/src/main/java/com/codevalley/envisionandroidassignment/network/NetworkHelper.kt
+++ app/src/main/java/com/codevalley/envisionandroidassignment/network/NetworkHelper.kt
@@ -8,13 +8,21 @@ import android.net.NetworkCapabilities
 class NetworkHelper(private val context: Context) {
     fun isNetworkConnected(): Boolean {
         var result = false
+        // register activity with the connectivity manager service
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        // Returns a Network object corresponding to
+        // the currently active default data network.
         val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        // Representation of the capabilities of an active network.
         val activeNetwork =
             connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
         result = when {
+            // Indicates this network uses a Wi-Fi transport,
+            // or WiFi has network connectivity
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            // Indicates this network uses a Cellular transport. or
+            // Cellular has network connectivity
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
             else -> false
